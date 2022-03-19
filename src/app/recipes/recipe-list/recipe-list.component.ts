81b96bf64e583d7e2c9588f19cc8e709f87f90df
Yauchin M. Lam <yauchinlam17@gmail.com>
2022-03-19 19:49:30 -0400

    Added content to recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 
+    'https://olddesignshop.com/wp-content/uploads/2013/08/OldDesignShop_PastryIgleheartPie1922.jpg')
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
