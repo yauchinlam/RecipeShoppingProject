cd6e35370c2c67151b4f85cccf5ae2e03599fd04
Yauchin M. Lam <yauchinlam17@gmail.com>
2022-05-01 22:44:52 -0400

    Added ingredients to the recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
@@ -6,10 +7,22 @@ export class RecipeService{
     public recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a test', 
-        'https://olddesignshop.com/wp-content/uploads/2013/08/OldDesignShop_PastryIgleheartPie1922.jpg'),
-        new Recipe('Another Test Recipe', 'This is a test', 
-        'https://olddesignshop.com/wp-content/uploads/2013/08/OldDesignShop_PastryIgleheartPie1922.jpg')
+        new Recipe(
+            'A Test Recipe', 
+            'This is a test', 
+        'https://olddesignshop.com/wp-content/uploads/2013/08/OldDesignShop_PastryIgleheartPie1922.jpg',
+        [
+            new Ingredient('Meat',1),
+            new Ingredient('apple',1)
+        ]
+        ),
+        new Recipe('Another Test Recipe', 
+        'This is a test', 
+        'https://olddesignshop.com/wp-content/uploads/2013/08/OldDesignShop_PastryIgleheartPie1922.jpg',
+        [
+            new Ingredient('apple',1),
+            new Ingredient('sugar',1)
+        ])
       ];
 
     getRecipes(){
